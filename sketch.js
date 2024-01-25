3f1b36cb16b70fb7e59a0005395c2b1847938942
motttey <motitago@gmail.com>
2024-01-25 21:47:09 +0900

    modify: add mode gameover

--- sketch.js
+++ sketch.js
@@ -5,33 +5,54 @@ let xSpeed = (2, 7);
 let ySpeed = (-7, -2);
 let score = 0;
 
+const bar_width = 90;
+const bar_height = 15;
+const width = 400;
+const height = 400;
+
 //Canvas
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(width, height);
 }
 
 //Background
 function draw() {
+    isGameOver = checkGameOver();
     //Background
     background(0);
 
-    //Paddle
-    fill('#ffffff');//#ffffff = BLACK
-    rect(mouseX, mouseY, 90, 15);
+    if (!isGameOver) {
+        //Paddle
+        fill('#ffffff');
+        rect(mouseX, mouseY, bar_width, bar_height);
 
 
-    //Functions
-    move();
-    display();
-    bounce();
-    //resetBall();
-    paddle();
+        //Functions
+        move();
+        display();
+        bounce();
+        //resetBall();
+        paddle();
+    } else {
+        //Score
+        fill('#ffffff');
+        textSize(24);
+        text("Game Over", width/2, height/2);
+    }
+
 
     //Score
     fill('#d9c3f7');
     textSize(24);
     text("Score: " + score, 10, 25);
+}
 
+function checkGameOver(){
+    if (yBall >= height) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function move(){
@@ -41,11 +62,10 @@ function move(){
 
 function bounce(){
     if(xBall < 10 ||
-        xBall > 400 - 10){
+        xBall > width - 10){
         xSpeed *= -1;
     }
-    if(yBall < 10 ||
-        yBall > 400 - 10){
+    if(yBall < 10){
         ySpeed *= -1;
     }
 }
@@ -58,7 +78,10 @@ function display(){
 //Bounce off paddle
 function paddle(){
     // 当たり判定
-    if((xBall > mouseX && xBall < mouseX + 90) && (xBall > mouseY && xBall < mouseY + 15)){
+    if (
+        (xBall > mouseX && xBall < mouseX + bar_width) && 
+        (xBall > mouseY && xBall < mouseY + bar_height)
+    ){
         xSpeed *= -1;
         ySpeed *= -1;
         score ++;
